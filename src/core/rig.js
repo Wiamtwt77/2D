3ddda2abc5cd576d523c8be09848447bed9dda1d
Wiamtwt77 <124047244+Wiamtwt77@users.noreply.github.com>
2026-08-15 18:17:08 +0200

    Update rig.js

--- src/core/rig.js
+++ src/core/rig.js
@@ -1,18 +1,13 @@
 export const rig = {
     bones: [
-        { id: 0, name: 'الحوض (Pelvis)', x: 425, y: 220, length: 0, parent: null },
-        { id: 1, name: 'الجذع (Torso)', x: 425, y: 160, length: 60, parent: 0 },
-        { id: 2, name: 'الرأس (Head)', x: 425, y: 100, length: 60, parent: 1 },
-        { id: 3, name: 'الكتف الأيمن (Right Shoulder)', x: 395, y: 150, length: 30, parent: 1 },
-        { id: 4, name: 'اليد اليمنى العليا (R. Upper Arm)', x: 360, y: 180, length: 45, parent: 3 },
-        { id: 5, name: 'اليد اليمنى السفلى (R. Lower Arm)', x: 330, y: 220, length: 45, parent: 4 },
-        { id: 6, name: 'الكتف الأيسر (Left Shoulder)', x: 455, y: 150, length: 30, parent: 1 },
-        { id: 7, name: 'اليد اليسرى العليا (L. Upper Arm)', x: 490, y: 180, length: 45, parent: 6 },
-        { id: 8, name: 'اليد اليسرى السفلى (L. Lower Arm)', x: 520, y: 220, length: 45, parent: 7 },
-        { id: 9, name: 'الفخذ الأيمن (Right Thigh)', x: 405, y: 230, length: 60, parent: 0 },
-        { id: 10, name: 'الساق اليمنى (Right Shin)', x: 405, y: 300, length: 70, parent: 9 },
-        { id: 11, name: 'الفخذ الأيسر (Left Thigh)', x: 445, y: 230, length: 60, parent: 0 },
-        { id: 12, name: 'الساق اليسرى (Left Shin)', x: 445, y: 300, length: 70, parent: 11 }
+        { id: 0, name: 'الجذع/الحوض (Root)', x: 425, y: 220, length: 0, parent: null },
+        { id: 1, name: 'الكتف الأيمن', x: 395, y: 150, length: 30, parent: 0 },
+        { id: 2, name: 'الاليد العليا (R. Upper Arm)', x: 350, y: 180, length: 50, parent: 1 },
+        { id: 3, name: 'اليد السفلى واليد (R. Hand)', x: 310, y: 210, length: 50, parent: 2 },
+        
+        { id: 4, name: 'الكتف الأيسر', x: 455, y: 150, length: 30, parent: 0 },
+        { id: 5, name: 'اليد اليسرى العليا (L. Upper Arm)', x: 500, y: 180, length: 50, parent: 4 },
+        { id: 6, name: 'اليد اليسرى السفلى (L. Hand)', x: 540, y: 210, length: 50, parent: 5 },
     ],
     textures: {}
 };
